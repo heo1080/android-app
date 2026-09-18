7e1ebf810d053065ef6e000ea0dca2289e735edd
heo1080 <heo1080@gmail.com>
2026-09-18 13:05:03 +0900

    Switch Next branch to clean nextapp module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,5 +12,5 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-rootProject.name = "DolphinAutoAssistant"
-include(":app")
+rootProject.name = "DolphinAssistantNext"
+include(":nextapp")
