6ec0a6663b8a2d4b648c74595f4bdedae9a5b16d
heo1080 <heo1080@gmail.com>
2026-09-18 13:24:09 +0900

    Move Next vehicle runtime into foreground service

--- nextapp/src/main/java/com/byd/dolphin/autoassistant/MainActivity.kt
+++ nextapp/src/main/java/com/byd/dolphin/autoassistant/MainActivity.kt
@@ -1,44 +1,35 @@
 package com.byd.dolphin.autoassistant
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.content.FileProvider
 import com.byd.dolphin.autoassistant.core.NextLog
-import com.byd.dolphin.autoassistant.core.audio.AlertEngine
+import com.byd.dolphin.autoassistant.core.NextRuntime
 import com.byd.dolphin.autoassistant.core.diagnostics.DiagnosticExporter
 import com.byd.dolphin.autoassistant.core.display.SplitController
-import com.byd.dolphin.autoassistant.core.event.VehicleEventEngine
-import com.byd.dolphin.autoassistant.core.state.VehicleStateStore
 import com.byd.dolphin.autoassistant.core.update.UpdateClient
+import com.byd.dolphin.autoassistant.service.DolphinNextService
 import com.byd.dolphin.autoassistant.ui.DolphinNextApp
 
 class MainActivity : ComponentActivity() {
-    private lateinit var stateStore: VehicleStateStore
-    private lateinit var alerts: AlertEngine
-    private lateinit var events: VehicleEventEngine
     private lateinit var updater: UpdateClient
     private lateinit var splitController: SplitController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        NextLog.init(this)
-        stateStore = VehicleStateStore(this)
-        alerts = AlertEngine(this)
-        events = VehicleEventEngine(stateStore, alerts)
+        NextRuntime.ensure(this)
         updater = UpdateClient(this)
         splitController = SplitController(this)
-
-        stateStore.start()
-        events.start()
-        alerts.ensureTtsModel()
+        startVehicleService()
 
         setContent {
             DolphinNextApp(
-                stateStore = stateStore,
-                alerts = alerts,
+                stateStore = NextRuntime.stateStore,
+                alerts = NextRuntime.alerts,
                 updater = updater,
                 splitController = splitController,
                 onShareDiagnostic = { shareDiagnostic() }
@@ -49,14 +40,25 @@ class MainActivity : ComponentActivity() {
     }
 
     override fun onDestroy() {
-        events.stop()
-        stateStore.stop()
-        alerts.release()
         super.onDestroy()
     }
 
+    private fun startVehicleService() {
+        val intent = Intent(this, DolphinNextService::class.java)
+        runCatching {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                startForegroundService(intent)
+            } else {
+                startService(intent)
+            }
+        }.onFailure {
+            NextLog.e("BOOT", "foreground vehicle service start failed", it)
+            NextRuntime.start(this)
+        }
+    }
+
     private fun shareDiagnostic() {
-        val file = DiagnosticExporter.create(this, stateStore.state.value, alerts)
+        val file = DiagnosticExporter.create(this, NextRuntime.stateStore.state.value, NextRuntime.alerts)
         val uri = FileProvider.getUriForFile(
             this,
             packageName + ".fileprovider",
