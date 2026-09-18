16a07afb6e3de16a1ffe931b0ec314d918e59fc9
heo1080 <heo1080@gmail.com>
2026-09-19 07:55:10 +0900

    Fix legacy TTS editor regen phrase key

--- app/src/main/java/com/byd/dolphin/autoassistant/MainActivity.kt
+++ app/src/main/java/com/byd/dolphin/autoassistant/MainActivity.kt
@@ -406,7 +406,7 @@ class MainActivity : AppCompatActivity() {
         setupVoiceEditButton(R.id.btnVoiceGearN, "N단 (중립)", "중립", "중립 기어 상태입니다. 브레이크 페달을 유지하세요.") { SettingsManager.getGearPhrase(this, "N") }
         setupVoiceEditButton(R.id.btnVoiceGearD, "D단 (전진)", "전진", "전진 기어가 체결되었습니다. 안전 운전하십시오.") { SettingsManager.getGearPhrase(this, "D") }
         setupVoiceEditButton(R.id.btnVoiceDriveMode, "드라이브 모드", "노멀 모드", "드라이브 모드가 정상적으로 전환되었습니다.") { SettingsManager.getDriveModePhrase(this, "NORMAL") }
-        setupVoiceEditButton(R.id.btnVoiceRegenMode, "회생 제동", "회생제동 에코", "회생제동 감속 제어가 적용되었습니다.") { SettingsManager.getRegenModePhrase(this, "ECO") }
+        setupVoiceEditButton(R.id.btnVoiceRegenMode, "회생 제동 STANDARD", "회생제동 스탠다드", "회생제동 스탠다드 모드입니다.") { SettingsManager.getRegenModePhrase(this, "STANDARD") }
 
         // 스노우모드 기본 문구는 사용자 요청에 따라 "스노우 모드"로 고정!
         setupVoiceEditButton(R.id.btnVoiceSnowMode, "스노우 모드", "스노우 모드", "노면 미끄럼 방지 스노우 모드가 작동합니다.") { SettingsManager.getSnowModePhrase(this) }
@@ -541,7 +541,7 @@ class MainActivity : AppCompatActivity() {
             R.id.btnVoiceGearN -> SettingsManager.setGearPhrase(this, "N", phrase)
             R.id.btnVoiceGearD -> SettingsManager.setGearPhrase(this, "D", phrase)
             R.id.btnVoiceDriveMode -> SettingsManager.setDriveModePhrase(this, "NORMAL", phrase)
-            R.id.btnVoiceRegenMode -> SettingsManager.setRegenModePhrase(this, "ECO", phrase)
+            R.id.btnVoiceRegenMode -> SettingsManager.setRegenModePhrase(this, "STANDARD", phrase)
             R.id.btnVoiceSnowMode -> SettingsManager.setSnowModePhrase(this, phrase)
             R.id.btnVoiceAutoHoldSwitchOn -> SettingsManager.setAutoHoldSwitchPhrase(this, true, phrase)
             R.id.btnVoiceAutoHoldSwitchOff -> SettingsManager.setAutoHoldSwitchPhrase(this, false, phrase)
