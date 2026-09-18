a10d1c7cff52c74e6d90b270e4606ab3373fc1d7
heo1080 <heo1080@gmail.com>
2026-09-18 23:55:38 +0900

    Fix diagnostic dialog scroll import

--- nextapp/src/main/java/com/byd/dolphin/autoassistant/next/ui/LauncherHomeUi.kt
+++ nextapp/src/main/java/com/byd/dolphin/autoassistant/next/ui/LauncherHomeUi.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
