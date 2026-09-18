8fb7a903d578357ea0fc378c595447aad39e9d13
heo1080 <heo1080@gmail.com>
2026-09-18 13:06:27 +0900

    Add Next vehicle state model

---
+++ nextapp/src/main/java/com/byd/dolphin/autoassistant/core/model/VehicleState.kt
@@ -0,0 +1,32 @@
+package com.byd.dolphin.autoassistant.core.model
+
+data class VehicleState(
+    val connected: Boolean = false,
+    val sampledAtMs: Long = 0L,
+    val speedKmh: Double? = null,
+    val gear: String? = null,
+    val gearRaw: Int? = null,
+    val epbApplied: Boolean? = null,
+    val epbRaw: Int? = null,
+    val driveMode: String? = null,
+    val driveModeRaw: Int? = null,
+    val regenMode: String? = null,
+    val regenRaw: Int? = null,
+    val snowMode: Boolean? = null,
+    val snowRaw: Int? = null,
+    val avhRaw: Int? = null,
+    val autoHoldHolding: Boolean? = null,
+    val brakeDepth: Int? = null,
+    val acceleratorDepth: Int? = null,
+    val iccActive: Boolean? = null,
+    val tjaRaw: Int? = null,
+    val bsdRaw: Int? = null,
+    val turnDirection: String? = null,
+    val turnRaw: Int? = null,
+    val frontLeftCm: Int? = null,
+    val frontRightCm: Int? = null,
+    val driverSeatHeat: Int? = null,
+    val passengerSeatHeat: Int? = null,
+    val steeringHeat: Boolean? = null,
+    val acOn: Boolean? = null
+)
