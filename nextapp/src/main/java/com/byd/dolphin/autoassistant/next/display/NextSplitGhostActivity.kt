72479314bced7aab9c68361021aa4a01f2c4c3a1
heo1080 <heo1080@gmail.com>
2026-09-18 23:45:35 +0900

    Add split-screen ghost activity for verified DiLink path

---
+++ nextapp/src/main/java/com/byd/dolphin/autoassistant/next/display/NextSplitGhostActivity.kt
@@ -0,0 +1,10 @@
+package com.byd.dolphin.autoassistant.next.display
+
+import android.app.Activity
+import android.os.Bundle
+
+class NextSplitGhostActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
