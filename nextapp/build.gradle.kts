ae67a9a7f79bb1d9733341fa8b6004539ecf04af
heo1080 <heo1080@gmail.com>
2026-09-18 22:01:53 +0900

    Bump DolphinAssistant to v34 launcher home

--- nextapp/build.gradle.kts
+++ nextapp/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.byd.dolphin.autoassistant"
         minSdk = 29
         targetSdk = 34
-        versionCode = 109
-        versionName = "6.1.0-v33.1.0-cluster-hub"
+        versionCode = 110
+        versionName = "7.0.0-v34.0.0-launcher-home"
     }
 
     buildTypes {
