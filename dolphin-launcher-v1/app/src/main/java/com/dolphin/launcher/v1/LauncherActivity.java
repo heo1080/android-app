53f6ad0a96bc195acdb6828ee9e3ed656f882a15
heo1080 <heo1080@gmail.com>
2026-09-25 15:52:14 +0900

    Run app orientation metadata scan on launcher start

--- dolphin-launcher-v1/app/src/main/java/com/dolphin/launcher/v1/LauncherActivity.java
+++ dolphin-launcher-v1/app/src/main/java/com/dolphin/launcher/v1/LauncherActivity.java
@@ -126,6 +126,7 @@ public class LauncherActivity extends Activity {
         VerificationEvidenceRuntime.startAutomaticUploadRuntime(this);
         VerificationEvidenceRuntime.recordPassiveEvent(this, "APP_LAUNCH", "LauncherActivity created");
         DisplayDiagnostics.capture(this);
+        DisplayDiagnostics.captureLaunchableAppOrientations(this);
         VerificationEvidenceRuntime.retryPendingUploadsAsync(this);
         startVehicleReadOnlyRuntime();
         handler.postDelayed(() -> {
